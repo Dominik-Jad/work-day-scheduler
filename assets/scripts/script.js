88c0d517f280ac0b0d7553d7b10d8e3c63c114f3
Dominik Jadczak <123301714+Dominik-Jad@users.noreply.github.com>
2024-01-22 19:02:00 +0000

    added function that color codes the time-blocks for past, present and future.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,54 +1,77 @@
 // when the document is ready, run the following function
 $(document).ready(function () {
-// display current day at top of the page 
-var today = dayjs().format('dddd D MMMM YYYY');
-$("#currentDay").text(today);
-
-// present time-blocks for standard business hours (9am-5pm)
-var workingHours = [
-    "9am",
-    "10am",
-    "11am",
-    "12pm",
-    "1pm",
-    "2pm",
-    "3pm",
-    "4pm",
-    "5pm"
-];
-
-function displayTimeBlocks() {
-         for (var i = 0; i < workingHours.length; i++) {
-            
-        var descriptionEl = $("<textarea>").addClass("description col-10");
-        var timeBlockEl = $("<div>").addClass("row time-block");
-        var hourEl = $("<div>").addClass("hour col-1");
-        var iconEl = $("<i>").addClass("fas fa-save");
-        var saveBtnEl = $("<button>").addClass("saveBtn col-1").append(iconEl);
-        
-        hourEl.attr("data-hour", parseInt(workingHours[i]));
-        descriptionEl.attr("data-hour", parseInt(workingHours[i]));
-        saveBtnEl.attr("data-hour", parseInt(workingHours[i]));
-
-        timeBlockEl.append(hourEl.text(workingHours[i]));
-        timeBlockEl.append(descriptionEl);
-        timeBlockEl.append(saveBtnEl);
-
-        $(".container").append(timeBlockEl);
+    // display current day at top of the page 
+    var today = dayjs().format('dddd D MMMM YYYY');
+    $("#currentDay").text(today);
+    // array of working hours 
+    var workingHours = [
+        "9AM",
+        "10AM",
+        "11AM",
+        "12PM",
+        "1PM",
+        "2PM",
+        "3PM",
+        "4PM",
+        "5PM"
+    ];
+    // present time-blocks for standard business hours (9am-5pm)
+    function displayTimeBlocks() {
+        for (var i = 0; i < workingHours.length; i++) {
+            // create elements for time-blocks
+            var descriptionEl = $("<textarea>").addClass("description col-9");
+            var timeBlockEl = $("<div>").addClass("row time-block");
+            var hourEl = $("<div>").addClass("hour col-1");
+            var iconSaveEl = $("<i>").addClass("fas fa-save");
+            var saveBtnEl = $("<button>").addClass("saveBtn col-1").append(iconSaveEl);
+            var iconDeleteEl = $("<i>").addClass("fas fa-trash");
+            var deleteBtnEl = $("<button>").addClass("deleteBtn col-1").append(iconDeleteEl);
+            // add data attributes to elements for each hour of the work day
+            hourEl.attr("data-hour", workingHours[i]);
+            descriptionEl.attr("data-hour", workingHours[i]);
+            saveBtnEl.attr("data-hour", workingHours[i]);
+            deleteBtnEl.attr("data-hour", workingHours[i]);
+            // append elements to the time-block and then appned that to the container
+            timeBlockEl.append(hourEl.text(workingHours[i]));
+            timeBlockEl.append(saveBtnEl, deleteBtnEl, descriptionEl);
+            $(".container").append(timeBlockEl);
+        }
     }
-}
-// color code time-blocks to indicate whether it is in the past, present, or future
-
-
-// user can enter an event in a time-block 
-
-
-// user can save event to local storage
-
-
-// saved events persist when page is refreshed
-function init() {
-    displayTimeBlocks();
-}
-init();
+    // color code time-blocks to indicate whether it is in the past, present, or future
+    function colorCode() {
+        // get current hour
+        var currentHour = dayjs().hour();
+        // get all time-blocks
+        var timeBlock = $(".time-block");
+        // loop through each time-block
+        timeBlock.each(function () {
+            // get the hour of the time-block
+            var hour = $(this).find(".hour").attr("data-hour");
+            // get the description of the time-block. 
+            var description = $(this).find(".description");
+            // convert the hour to a number from e.g "9AM" to 9
+            var hourNum = parseInt(hour);
+            // if the hour is in the afternoon, add 12 to the hourNum to convert it to 24 hour time
+            // leave 12PM as 12
+            if (hour.includes("PM") && hourNum !== 12) {
+                hourNum += 12;
+            }
+            // compare the hourNum to the currentHour and apply the appropriate class
+            if (hourNum < currentHour) {
+                description.addClass("past");
+            } else if (hourNum === currentHour) {
+                description.addClass("present");
+            } else {
+                description.addClass("future");
+            }
+        });
+    }
+    // user can enter an event in a time-block 
+   
+    // saved events persist when page is refreshed
+    function init() {
+        displayTimeBlocks();
+        colorCode();
+    }
+    init();
 });
